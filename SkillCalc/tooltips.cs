42acc6203a9455bc71a69375c8b3b8a506424ee8
stheno <turbod_speedy@hotmail.com>
2015-05-18 03:01:22 -0700

    Attached License

--- SkillCalc/tooltips.cs
+++ SkillCalc/tooltips.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿/*
+SkillCalc Copyright (c) sstheno@gmail.com
+
+This file is part of SkillCalc.
+
+    SkillCalc is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SkillCalc is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SkillCalc.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
